00aa13d0beead94e45aff7ad7012a378b53abbfa
stkjoe <28199363+stkjoe@users.noreply.github.com>
2019-07-30 23:59:15 +1200

    basic upload file template

--- flasksite.py
+++ flasksite.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+app.config["UPLOAD_FOLDER"] = "temp"
 
 @app.route("/")
 @app.route("/home")
@@ -14,6 +19,13 @@ def about():
 @app.route("/app")
 def modmymap():
     return render_template("app.html")
+
+@app.route("/app", methods=["POST"])
+def send_file():
+    fileob = request.files["file2upload"]
+    filename = secure_filename(fileob.filename)
+    save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
+    fileob.save(save_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -18,3 +18,63 @@ function changeTab(evt, tabName) {
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+//
+// initialize
+window.onload = function () {
+
+	var filedrag = document.getElementById("dragdrop");
+    var mainwindow = document.getElementsByTagName("main")[0];
+
+	// is XHR2 available?
+	var xhr = new XMLHttpRequest();
+	if (xhr.upload) {
+
+		// file drop
+		filedrag.addEventListener("dragover", FileDragHover, false);
+		filedrag.addEventListener("dragleave", FileDragHover, false);
+		filedrag.addEventListener("drop", FileSelectHandler, false);
+		filedrag.style.zIndex = 1;
+        mainwindow.addEventListener("dragover", FileDragVis, false);
+	}
+};
+
+function FileDragVis(e) {
+    var filedrag = document.getElementById("dragdrop");
+    filedrag.style.zIndex = 1;
+}
+
+// file drag hover
+function FileDragHover(e) {
+	e.stopPropagation();
+	e.preventDefault();
+	e.target.className = (e.type == "dragover" ? "hover" : "");
+}
+
+// file selection
+function FileSelectHandler(e) {
+    var filedrag = document.getElementById("dragdrop");
+    filedrag.style.zIndex = -1;
+
+	// cancel event and hover styling
+	FileDragHover(e);
+
+	// fetch FileList object
+	var files = e.target.files || e.dataTransfer.files;
+
+    // process all File objects
+    var formData = new FormData();
+    formData.append("file2upload", files[0]);
+
+    var req = {
+        url: "/app",
+        method: "post",
+        processData: false,
+        contentType: false,
+        data: formData
+    };
+
+    var promise = $.ajax(req);
+
+
+}
