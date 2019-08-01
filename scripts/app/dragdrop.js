86c5c81e729d3064e4bbca12a18dcdae0ec9e1cf
stkjoe <28199363+stkjoe@users.noreply.github.com>
2019-08-01 20:39:20 +1200

    fix current scripts

--- scripts/app/dragdrop.js
+++ scripts/app/dragdrop.js
@@ -1,63 +1,34 @@
+function dropHandler(ev) {
 
-//
-// initialize
-function dragdrop() {
+	ev.preventDefault();
 
-	var filedrag = document.getElementById("dragdrop");
-    var mainwindow = document.getElementsByTagName("main")[0];
-
-	// is XHR2 available?
-	var xhr = new XMLHttpRequest();
-	if (xhr.upload) {
-
-		// file drop
-		filedrag.addEventListener("dragover", FileDragHover, false);
-		filedrag.addEventListener("dragleave", FileDragHover, false);
-		filedrag.addEventListener("drop", FileSelectHandler, false);
-        mainwindow.addEventListener("dragover", FileDragVis, false);
-	}
-};
-
-function FileDragVis(e) {
-    var filedrag = document.getElementById("dragdrop");
-    filedrag.style.zIndex = 1;
+	if (ev.dataTransfer.items) {
+		for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+			if (ev.dataTransfer.items[i].kind === 'file') {
+				var file = ev.dataTransfer.items[i].getAsFile();
+				var isOsz = file.name.endsWith(".osz");
+				if (isOsz == true) {
+					// File is a .osz file. Continue onwards.
+					/*
+					var returnedJSON = modmymap.results;
+					sessionStorage.setItem(file.name, returnedJSON);
+					*/
+				}
+			}
+		}
+    }
+	var dropzone = document.getElementById("dragdrop");
+	dropzone.style.zIndex = -1;
 }
 
-// file drag hover
-function FileDragHover(e) {
-	e.stopPropagation();
-	e.preventDefault();
-	e.target.className = (e.type == "dragover" ? "hover" : "");
+function dragOverHandler(ev) {
+	var dropzone = document.getElementById("dragdrop");
+	dropzone.style.zIndex = 1;
+	ev.preventDefault();
 }
 
-// file selection
-function FileSelectHandler(e) {
-    var filedrag = document.getElementById("dragdrop");
-    filedrag.style.zIndex = -1;
-
-	// cancel event and hover styling
-	FileDragHover(e);
-
-	// fetch FileList object
-	var files = e.target.files || e.dataTransfer.files;
-
-    // process all File objects
-    var formData = new FormData();
-    formData.append("file2upload", files[0]);
-
-    var req = {
-        url: "/app",
-        method: "post",
-        processData: false,
-        contentType: false,
-        data: formData
-    };
-
-    var promise = $.ajax(req);
-}
-
-if (window.addEventListener) {
-    window.addEventListener('load', dragdrop);
-} else if (window.attachEvent) {
-    window.attachEvent('onload', dragdrop);
+function dragLeaveHandler(ev) {
+	var dropzone = document.getElementById("dragdrop");
+	dropzone.style.zIndex = -1;
+	ev.preventDefault();
 }
--- scripts/app/treeview.js
+++ scripts/app/treeview.js
@@ -1,11 +1,11 @@
 function treeview() {
-    var toggler = document.getElementsByClassName("caret");
+    var toggler = document.getElementsByClassName("treeroot");
     var i;
 
     for (i = 0; i < toggler.length; i++) {
         toggler[i].addEventListener("click", function() {
             this.parentElement.querySelector(".nested").classList.toggle("activetree");
-            this.classList.toggle("caret-down");
+            this.classList.toggle("treeroot-down");
         });
     }
 }
