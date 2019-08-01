4c310eab773ee13c697d8a76f44ef3de5d3a096c
stkjoe <28199363+stkjoe@users.noreply.github.com>
2019-08-02 01:49:19 +1200

    currently trying to fix slider value bug

--- scripts/app/management.js
+++ scripts/app/management.js
@@ -3,6 +3,36 @@ var beatmapsetManagement = (function () {
 
     pub.currentBeatmapset = "";
 
+    pub.settingsChange = function() {
+        var target = document.getElementById("diffs");
+        var settings = sessionStorage.getItem(beatmapsetManagement.currentBeatmapset + "-info");
+        console.log(settings);
+        settings = JSON.parse(settings);
+        console.log(snap4.value);
+        console.log(settings);
+        var detdiff = document.getElementById("diffsdetect").value;
+        var snap3 = document.getElementById("snap3").value;
+        if (snap3 == "2") {
+            snap3 = "3";
+        } else if (snap3 == "3") {
+            snap3 = "6";
+        } else if (snap3 == "4") {
+            snap3 = "12";
+        }
+        var snap3 = document.getElementById("snap4").value;
+        if (snap4 == "3") {
+            snap4 = "4";
+        } else if (snap4 == "4") {
+            snap4 = "8";
+        } else if (snap4 == "5") {
+            snap4 = "16";
+        }
+        settings[target.value]["diff"] = detdiff;
+        settings[target.value]["snap3"] = snap3;
+        settings[target.value]["snap4"] = snap4;
+        sessionStorage.setItem(beatmapsetManagement.currentBeatmapset + "-info", JSON.stringify(settings));
+    };
+
     pub.add = function(name) {
         // Make new cell for beatmapset
         var target = document.getElementById("beatmapsets");
@@ -11,6 +41,12 @@ var beatmapsetManagement = (function () {
         target.appendChild(newCell);
         // Associate actions with new cell
         target.lastChild.firstChild.addEventListener('click', function() {
+            // visual indication of selected beatmapset
+            cells = document.getElementById("beatmapsets").getElementsByTagName("tr");
+            for (i = 0; i < cells.length; i++) {
+                cells[i].className = "";
+            }
+            newCell.className = "active"
             beatmapsetManagement.switch(name);
         });
         target.lastChild.lastChild.addEventListener('click', function() {
@@ -19,22 +55,93 @@ var beatmapsetManagement = (function () {
                 document.getElementsByClassName("settings")[0].style.pointerEvents = "none";
             }
         });
-        //pub.switch(name);
+
+        // Create a new info sessionstorage file to keep track of snapping settings
+        var text = sessionStorage.getItem(name);
+        var returnedJSON = JSON.parse(text);
+        var settings = {};
+        Object.keys(returnedJSON.diffs).forEach(function(k){
+            var diff = returnedJSON.diffs[k].diff;
+            var snap3 = "";
+            var snap4 = "";
+            if (diff == "easy") {
+                snap3 = "1";
+                snap4 = "2";
+            } else if (diff == "normal") {
+                snap3 = "1";
+                snap4 = "2";
+            } else if (diff == "hard") {
+                snap3 = "3";
+                snap4 = "4";
+            } else if (diff == "insane") {
+                snap3 = "3";
+                snap4 = "4";
+            } else if (diff == "extra") {
+                snap3 = "6";
+                snap4 = "8";
+            }
+            settings[k] = {"diff": diff, "snap3": snap3, "snap4": snap4};
+        });
+        settings = JSON.stringify(settings);
+        console.log(settings);
+        sessionStorage.setItem(name + "-info", settings);
+
+        // reset cell colours
+        cells = document.getElementById("beatmapsets").getElementsByTagName("tr");
+        for (i = 0; i < cells.length; i++) {
+            cells[i].className = "";
+        }
+        newCell.className = "active"
+        pub.switch(name);
     };
 
     pub.switch = function(name) {
         // clear current information
-        document.getElementById("beatmapinfo").innerHTML = "";
-        document.getElementById("probleminfo").innerHTML = "";
-        document.getElementById("problemtree").innerHTML = "";
+        //document.getElementById("beatmapinfo").innerHTML = "";
+        //document.getElementById("probleminfo").innerHTML = "";
+        //document.getElementById("problemtree").innerHTML = "";
         document.getElementById("diffs").innerHTML = "";
 
         // get clicked beatmapset
-        var json = sessionStorage.getItem(name);
+        var text = sessionStorage.getItem(name);
+        var returnedJSON = JSON.parse(text);
         var i;
 
         // populate info
-
+        // start by addings diffs
+        var target = document.getElementById("diffs");
+        Object.keys(returnedJSON.diffs).forEach(function(k){
+            target.innerHTML = target.innerHTML + "<option value='" + k + "'>" + k + "</option>"
+        });
+        target.addEventListener("change", function() {
+            // Grab settings from sessionStorage
+            var settings = JSON.parse(sessionStorage.getItem(name + "-info"))[target.value];
+            console.log(settings);
+            document.getElementById("diffsdetect").value = settings["diff"];
+            var snap3 = settings["snap3"];
+            document.getElementById("snap3label").innerHTML = "1/" + snap3.toString();
+            if (snap3 == "3") {
+                snap3 = "2";
+            } else if (snap3 == "6") {
+                snap3 = "3";
+            } else if (snap3 == "12") {
+                snap3 = "4";
+            }
+            document.getElementById("snap3").value = snap3;
+            var snap4 = settings["snap4"];
+            document.getElementById("snap4label").innerHTML = "1/" + snap4.toString();
+            if (snap4 == "4") {
+                snap4 = "3";
+            } else if (snap4 == "8") {
+                snap4 = "4";
+            } else if (snap4 == "16") {
+                snap4 = "5";
+            }
+            document.getElementById("snap4").value = snap4;
+        });
+        // Change to hardest diff and evoke change
+        target.selectedIndex = target.length - 1;
+        target.dispatchEvent(new CustomEvent("change"));
 
         // reset current settings values
         document.getElementsByClassName("settings")[0].style.pointerEvents = "auto";
--- scripts/app/modmymap.js
+++ scripts/app/modmymap.js
@@ -15,7 +15,6 @@ function modMyMap(file) {
     document.getElementsByClassName("settings")[0].style.pointerEvents = "auto";
 
     // delete below
-    var text = '{"report":{"Unrankables":{"General":{"Missing Preview Point":["Normal"],"MP3 Out of Range":["audio.mp3 - 320kbps"]},"Easy":{"Concurrent Objects":["00:00:001 - ","00:00:002 -"],"Missing Background":["badbackground.jpg","alt.jpg"]}},"Guidelines":{"General":{"Missing Preview Point":["Normal"],"MP3 Out of Range":["audio.mp3 - 320kbps"]},"Easy":{"Concurrent Objects":["00:00:001 - ","00:00:002 -"],"Missing Background":["badbackground.jpg","alt.jpg"]}}},"diffs":{"Easy":{"info":"","snaps":{"1":["a"],"2":["b"],"3":["c"],"4":["d","e"],"6":["f"],"8":["g"],"12":["h"],"16":["i"]}},"Normal":{"info":"","snaps":{"1":["j"],"2":["k"],"3":["l"],"4":["m"],"6":["n","o"],"8":["p"],"12":["q"],"16":["r"]}},"Hard":{"info":"","snaps":{"1":["0"],"2":["1"],"3":["2","3"],"4":["4"],"6":["5"],"8":["6"],"12":["7"],"16":["8"]}}}}'
-    var obj = JSON.parse(text);
-    return obj
+    var text = '{"report":{"Unrankables":{"General":{"Missing Preview Point":["Normal"],"MP3 Out of Range":["audio.mp3 - 320kbps"]},"Easy":{"Concurrent Objects":["00:00:001 - ","00:00:002 -"],"Missing Background":["badbackground.jpg","alt.jpg"]}},"Guidelines":{"General":{"Missing Preview Point":["Normal"],"MP3 Out of Range":["audio.mp3 - 320kbps"]},"Easy":{"Concurrent Objects":["00:00:001 - ","00:00:002 -"],"Missing Background":["badbackground.jpg","alt.jpg"]}}},"diffs":{"Easy":{"info":"","diff":"easy","snaps":{"1":["a"],"2":["b"],"3":["c"],"4":["d","e"],"6":["f"],"8":["g"],"12":["h"],"16":["i"]}},"Normal":{"info":"","diff":"normal","snaps":{"1":["j"],"2":["k"],"3":["l"],"4":["m"],"6":["n","o"],"8":["p"],"12":["q"],"16":["r"]}},"Hard":{"info":"","diff":"hard","snaps":{"1":["0"],"2":["1"],"3":["2","3"],"4":["4"],"6":["5"],"8":["6"],"12":["7"],"16":["8"]}}}}'
+    return text
 }
