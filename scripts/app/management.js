cb387184f2a2b0e985067542469b46c77730412c
stkjoe <28199363+stkjoe@users.noreply.github.com>
2019-08-01 23:30:36 +1200

    basic UI lockout + enable when there is nothing loaded.

--- scripts/app/management.js
+++ scripts/app/management.js
@@ -2,7 +2,20 @@ var beatmapsetManagement = (function () {
     var pub = {};
 
     pub.add = function(name) {
-        pub.switch(name);
+        // Make new cell for beatmapset
+        var target = document.getElementById("beatmapsets");
+        var newCell = document.createElement("tr");
+        newCell.innerHTML = "<td>" + name + "</td><td>x</td>";
+        target.appendChild(newCell);
+        // Associate actions with new cell
+        target.lastChild.style.cursor = 'pointer';
+        target.lastChild.addEventListener('click', function() {
+            beatmapsetManagement.switch(name);
+        });
+        target.lastChild.lastChild.lastChild.addEventListener('click', function() {
+            beatmapsetManagement.switch(name);
+        });
+        //pub.switch(name);
     };
 
     pub.switch = function(name) {
